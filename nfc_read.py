9a0bc254ef4320ba6672d4dc5c17c9e0f55d930f
Hasan Yıldız <hasanyldz483@gmail.com>
2026-03-27 15:14:19 +0300

    Update nfc_read.py

--- nfc_read.py
+++ nfc_read.py
@@ -1,68 +1,16 @@
-from smbus2 import SMBus
-import time
+#!/usr/bin/env python3
+import board
+import busio
+from adafruit_pn532.i2c import PN532_I2C
 
-PN532_ADDR = 0x24
+i2c = busio.I2C(board.SCL, board.SDA)
+pn532 = PN532_I2C(i2c, debug=False)
 
-def write_command(bus, cmd, params=[]):
-    frame = [0x00, 0x00, 0xFF]
-    length = len(params) + 2
-    frame += [length, (~length & 0xFF), 0xD4, cmd]
-    frame += params
+pn532.SAM_configuration()
 
-    checksum = 0xD4 + cmd + sum(params)
-    frame += [(~checksum & 0xFF), 0x00]
+print("NFC kartını okutun...")
 
-    bus.write_i2c_block_data(PN532_ADDR, 0x00, frame)
-
-def read_ack(bus):
-    time.sleep(0.1)
-    ack = bus.read_i2c_block_data(PN532_ADDR, 0x00, 6)
-    return ack == [0x00, 0x00, 0xFF, 0x00, 0xFF, 0x00]
-
-def read_response(bus, length=32):
-    time.sleep(0.2)
-    return bus.read_i2c_block_data(PN532_ADDR, 0x00, length)
-
-def sam_config(bus):
-    write_command(bus, 0x14, [0x01, 0x14, 0x01])
-    read_ack(bus)
-
-def read_uid(bus):
-    write_command(bus, 0x4A, [0x01, 0x00])
-    if not read_ack(bus):
-        return None
-
-    data = read_response(bus)
-
-    # DEBUG için:
-    print("RAW:", data)
-
-    # UID parse (daha doğru offset)
-    try:
-        if data[7] != 0xD5:
-            return None
-        if data[8] != 0x4B:
-            return None
-
-        uid_length = data[13]
-        uid = data[14:14+uid_length]
-        return uid
-    except:
-        return None
-
-def main():
-    bus = SMBus(1)
-
-    print("PN532 init...")
-    sam_config(bus)
-
-    print("Kart okut...")
-
-    while True:
-        uid = read_uid(bus)
-        if uid:
-            print("KART ID:", [hex(x) for x in uid])
-            time.sleep(1)
-
-if __name__ == "__main__":
-    main()
+while True:
+    uid = pn532.read_passive_target(timeout=0.5)
+    if uid:
+        print("Kart ID:", uid.hex())
